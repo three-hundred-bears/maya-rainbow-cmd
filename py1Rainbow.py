c5f621a231524eb8b9c610ff675a8a8f5b5747c3
three-hundred-bears <125211414+three-hundred-bears@users.noreply.github.com>
2024-05-31 17:57:11 -0400

    Update py1Rainbow.py
    Added type annotations.

--- py1Rainbow.py
+++ py1Rainbow.py
@@ -11,13 +11,13 @@ kBoringLongFlag = "-boring"
 
 class Py1RainbowCmd(ompx.MPxCommand):
     """py1Rainbow command class."""
-    def __init__(self):
+    def __init__(self) -> None:
         super(Py1RainbowCmd, self).__init__()
-        self.boring = False
-        self.__dag_map = dict()
-        self.__iter = None
+        self.boring: bool = False
+        self.__dag_map: dict = dict()
+        self.__iter: om.MItDag | om.MItSelectionList = None
     
-    def doIt(self, args):
+    def doIt(self, args: om.MArgList):
         """Perform initial command operation.
         
         Retrieve any flags provided to command, set ocmmand iterator
@@ -31,7 +31,7 @@ class Py1RainbowCmd(ompx.MPxCommand):
             args: MArgList object storing optional args provided to command.
         """
 
-        arg_data = om.MArgDatabase(self.syntax(), args)
+        arg_data: om.MArgDatabase = om.MArgDatabase(self.syntax(), args)
         if arg_data.isFlagSet(kBoringFlag):
             self.boring = True
         
@@ -39,29 +39,29 @@ class Py1RainbowCmd(ompx.MPxCommand):
         self._iterate()
         self.redoIt()
     
-    def redoIt(self):
+    def redoIt(self) -> None:
         """Assign new color to objects mapped by doIt()."""
 
         for name, colors in self.__dag_map.items():
             cmds.color(name, rgbColor=colors[1])
     
-    def undoIt(self):
+    def undoIt(self) -> None:
         """Assign old color to objects mapped by doIt()."""
 
         for name, colors in self.__dag_map.items():
             cmds.color(name, rgbColor=colors[0])
     
-    def _set_iter(self):
+    def _set_iter(self) -> None:
         """Determine type of iterator based on command input."""
 
-        selection_list = om.MSelectionList()
+        selection_list: om.MSelectionList = om.MSelectionList()
         om.MGlobal.getActiveSelectionList(selection_list)
         if not selection_list.length():
-            self.__iter = om.MItDag(om.MItDag.kDepthFirst, om.MFn.kMesh)
+            self.__iter: om.MItDag = om.MItDag(om.MItDag.kDepthFirst, om.MFn.kMesh)
         else:
-            self.__iter = om.MItSelectionList(selection_list, om.MFn.kDagNode)
+            self.__iter: om.MItSelectionList = om.MItSelectionList(selection_list, om.MFn.kDagNode)
     
-    def _register_dag_obj(self, dag_func):
+    def _register_dag_obj(self, dag_func: om.MFnDagNode) -> None:
         """Register current iterator item in command class.
         
         Maps object name to tuple containing old and new object color within
@@ -74,13 +74,13 @@ class Py1RainbowCmd(ompx.MPxCommand):
                       iterator item.
         """
 
-        name = dag_func.name()
-        obj_color = dag_func.objectColorRGB()
-        old_clr = (obj_color.r, obj_color.g, obj_color.b)
-        new_clr = random_grayscale() if self.boring else random_color()
+        name: str = dag_func.name()
+        obj_color: om.MColor = dag_func.objectColorRGB()
+        old_clr: tuple[om.MColor] = (obj_color.r, obj_color.g, obj_color.b)
+        new_clr: tuple[float] = random_grayscale() if self.boring else random_color()
         self.__dag_map[name] = (old_clr, new_clr)
     
-    def _convert_path(self, dag_path):
+    def _convert_path(self, dag_path: om.MDagPath) -> om.MObject:
         """Expand current MItSelectionList item to DAG object.
         
         Args:
@@ -91,51 +91,51 @@ class Py1RainbowCmd(ompx.MPxCommand):
         self.__iter.getDagPath(dag_path)
         return dag_path.node()
     
-    def _iterate(self):
+    def _iterate(self) -> None:
         """Iterate over relevant items, registering them within __dag_map.
         
         Iterates through items stored in __iter, registering them in__dag_map
         to be accesed by redoIt() and undoIt().
         """
 
-        dag_path = om.MDagPath()
-        dag_func = om.MFnDagNode()
-        convert = False if isinstance(self.__iter, om.MItDag) else True
+        dag_path: om.DagPath = om.MDagPath()
+        dag_func: om.MFnDagNode = om.MFnDagNode()
+        convert: bool = False if isinstance(self.__iter, om.MItDag) else True
 
         while not self.__iter.isDone():
-            dag_obj = self._convert_path(dag_path) if convert else self.__iter.currentItem()
+            dag_obj: om.MObject = self._convert_path(dag_path) if convert else self.__iter.currentItem()
             dag_func.setObject(dag_obj)
             self._register_dag_obj(dag_func)
             self.__iter.next()
 
 
-def random_color():
+def random_color() -> tuple[float]:
     """Make random tuple3 composed of floats from 0 - 1."""
     return tuple(random.random() for _ in range(3))
 
 
-def random_grayscale():
+def random_grayscale() -> tuple[float]:
     """Make random tuple3 composed of a single float from 0 - 1."""
     val = random.random()
     return val, val, val
 
 
-def isUndoable():
+def isUndoable() -> bool:
     return True
 
 
-def cmdCreator():
+def cmdCreator() -> ompx.pointer:
     return ompx.asMPxPtr(Py1RainbowCmd())
 
 
-def syntaxCreator():
-    syntax = om.MSyntax()
+def syntaxCreator() -> om.MSyntax:
+    syntax: om.MSyntax = om.MSyntax()
     syntax.addFlag(kBoringFlag, kBoringLongFlag)
     return syntax
 
 
-def initializePlugin(plugin):
-    plugin_func = ompx.MFnPlugin(plugin)
+def initializePlugin(plugin: om.MObject) -> None:
+    plugin_func: om.MFnPlugin = ompx.MFnPlugin(plugin)
     try:
         plugin_func.registerCommand(
             kPluginCmdName, cmdCreator, syntaxCreator
@@ -145,8 +145,8 @@ def initializePlugin(plugin):
         raise
 
 
-def uninitializePlugin(plugin):
-    plugin_func = ompx.MFnPlugin(plugin)
+def uninitializePlugin(plugin) -> None:
+    plugin_func: ompx.MFnPlugin = ompx.MFnPlugin(plugin)
     try:
         plugin_func.deregisterCommand(kPluginCmdName)
     except:
